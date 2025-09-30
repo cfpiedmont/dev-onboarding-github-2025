e001234d754584909d1087a802a17933e8ea5388
Will Soylemez <57220048+William-Soylemez@users.noreply.github.com>
2025-09-30 19:43:00 -0400

    Add files via upload

---
+++ dev-onboarding-github-2024-main/dev-roster-website/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center'>JumboCode Team Roster</h1>
+      <div className='margin-left-1'>
+        <h2>A2Empowerment</h2>
+        <ul>
+          <li>Test Developer Name</li>
+          <li>Test Developer Name</li>
+        </ul>
+        <h2>Bread & Roses</h2>
+        <ul>
+          <li>Test Developer Name</li>
+          <li>Test Developer Name</li>
+        </ul>
+        <h2>Dillar Academy</h2>
+        <ul>
+          <li>Test Developer Name</li>
+          <li>Test Developer Name</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
